0892c805f08a790b616adf76c38138d22403357e
jdl582 <jdl582@nau.edu>
2025-11-02 23:37:01 -0700

    updates

---
+++ Site/index.js
@@ -0,0 +1,23 @@
+const articles = [
+    { title: "Breaking News: Headline 1", img: "https://via.placeholder.com/300x200" },
+    { title: "Breaking News: Headline 2", img: "https://via.placeholder.com/300x200" },
+    { title: "Breaking News: Headline 3", img: "https://via.placeholder.com/300x200" },
+    { title: "Breaking News: Headline 4", img: "https://via.placeholder.com/300x200" },
+];
+
+const container = document.getElementById("more-articles");
+
+articles.forEach(article => {
+    const div = document.createElement("div");
+    div.className = "col-md-3 mb-4";
+    div.innerHTML = `
+        <div class="card">
+            <img src="${article.img}" class="card-img-top" alt="${article.title}">
+            <div class="card-body">
+                <h5 class="card-title">${article.title}</h5>
+                <a href="#" class="btn btn-sm btn-primary">Read More</a>
+            </div>
+        </div>
+    `;
+    container.appendChild(div);
+});

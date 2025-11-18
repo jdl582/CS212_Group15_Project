87fbb7fa81232777558d71f4de98419b32c8d15b
Jake-Lawt <jakelawton72@yahoo.com>
2025-11-17 22:42:44 -0700

    added connor's changes

---
+++ Site/market.js
@@ -0,0 +1,31 @@
+$(function(){
+    $("#navbar-container").load("heading.html");
+});
+
+const articles = [
+    { 
+        title: "Health News Today", 
+        img: "HealthImage.jpg", 
+        link: "health.html" 
+    },
+    { 
+        title: "Sports Updates", 
+        img: "sportsImage.jpg", 
+        link: "sports.html" 
+    },
+    { 
+        title: "Culture Spotlight", 
+        img: "cultureImage.jpg", 
+        link: "culture.html" 
+    },
+    { 
+        title: "Market News", 
+        img: "marketImage.jpg", 
+        link: "market.html" 
+    },
+    {
+        title: "Business Insights",
+        img: "buisnessImage.jpg",
+        link: "business.html"
+    }
+];
